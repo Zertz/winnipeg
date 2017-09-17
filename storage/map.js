e34a10a3987269afecc0a57fc0f8c161e8e2516c
Pier-Luc Gendreau <pierluc@classcraft.com>
2017-09-17 10:52:45 -0400

    restructure storage provider

--- storage/map.js
+++ storage/map.js
@@ -1,10 +1,12 @@
-const map = new Map()
+module.exports = function () {
+  const map = new Map()
 
-module.exports = {
-  get (key) {
-    return map.get(key)
-  },
-  set (key, value) {
-    return map.set(key, value)
+  return {
+    get (key) {
+      return map.get(key)
+    },
+    set (key, value) {
+      return map.set(key, value)
+    }
   }
 }
