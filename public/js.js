72b0e722475dd2cd4202cd174aed8a2c53d55364
Pier-Luc Gendreau <pierluc@classcraft.com>
2017-09-22 18:08:29 -0400

    toggle loading animation

--- public/js.js
+++ public/js.js
@@ -6,30 +6,38 @@
   form.addEventListener('submit', async (e) => {
     e.preventDefault()
 
-    url.readOnly = true;
+    form.classList.add('loading')
+
+    url.readOnly = true
 
     short.style.visibility = 'hidden'
     short.value = ''
 
-    const response = await window.fetch('/', {
-      method: 'POST',
-      mode: 'same-origin',
-      cache: 'no-cache',
-      headers: new window.Headers({
-        'Content-Type': 'application/json'
-      }),
-      body: JSON.stringify({
-        url: url.value
+    try {
+      const response = await window.fetch('/', {
+        method: 'POST',
+        mode: 'same-origin',
+        cache: 'no-cache',
+        headers: new window.Headers({
+          'Content-Type': 'application/json'
+        }),
+        body: JSON.stringify({
+          url: url.value
+        })
       })
-    })
 
-    const json = await response.json()
+      const json = await response.json()
 
-    url.readOnly = false;
-    url.value = ''
+      short.style.visibility = 'visible'
+      short.value = json.short
+      short.select()
 
-    short.style.visibility = 'visible'
-    short.value = json.short
-    short.select()
+      url.readOnly = false
+      url.value = ''
+    } catch (err) {
+      console.error(err)
+    } finally {
+      form.classList.remove('loading')
+    }
   })
 })()
